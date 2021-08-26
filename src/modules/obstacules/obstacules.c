461ad0782a5a637780a5fff49ddf59e126ef74a1
Breno Cavalcanti <breno.uc.cavalcanti@gmail.com>
2021-08-26 20:49:08 -0300

    feat(add_base_for...): adicionado metodo reset e esboço do metodo objetivo.

--- src/modules/obstacules/obstacules.c
+++ src/modules/obstacules/obstacules.c
@@ -2,56 +2,102 @@
 #include <stdlib.h>
 #include <time.h>
 
-Rectangle* obstacules_init(int number_of_max_obstacules) {
-    int obstacules_max_number_of_obstacules = number_of_max_obstacules;
-    int obstacules_number_of_obstacules = 0;
-    Rectangle* obstacules_obstacules = (Rectangle*) malloc(sizeof(Rectangle) * obstacules_max_number_of_obstacules);
-    
-    for (int i = 0; i < obstacules_max_number_of_obstacules; i++) {
-        obstacules_obstacules[i].x = 1200;
-        obstacules_obstacules[i].y = -40;
-        obstacules_obstacules[i].width = 20;
-        obstacules_obstacules[i].height = 20;
+
+typedef struct {
+    float x;
+    float y;
+} Objective;
+typedef struct {
+    Rectangle rect;
+    Objective objective;
+    int is_active;
+} Obstacule;
+
+Obstacule* obstacules_init(int number_of_max_obstacules) {
+    Obstacule* obstacules = malloc(sizeof(Obstacule) * number_of_max_obstacules);
+    for (int i = 0; i < number_of_max_obstacules; i++) {
+        obstacules[i].rect.x = 900;
+        obstacules[i].rect.y = 300 *(i + 1);
+        obstacules[i].rect.width = 40;
+        obstacules[i].rect.height = 40;
+        obstacules[i].objective.x = -1;
+        obstacules[i].objective.y = -1;
+        obstacules[i].is_active = 0;
     }
-    return obstacules_obstacules;    
+    return obstacules;    
 }
 
-void render_obstacules(Rectangle* obstacules_obstacules, int obstacules_number_of_obstacules) {
+void render_obstacules(Obstacule* obstacules_obstacules, int obstacules_number_of_obstacules) {
     for (int i = 0; i < obstacules_number_of_obstacules; i++) {
-        DrawRectangleRec(obstacules_obstacules[i], RED);
-    }
-}
+        if(i == 0) {
+            DrawRectangleRec(obstacules_obstacules[i].rect, RED);
+            // printf("%f\n", obstacules_obstacules[i].y);
+        } else {
+            DrawRectangleRec(obstacules_obstacules[i].rect, GREEN);
 
-float* create_action_obstacule(Rectangle actual_obs, float player_x, float player_y) {
-    float* action_obstacule = (float*) malloc(sizeof(float) * 2);
-    action_obstacule[0] = 0;
-    action_obstacule[1] = 0;
-    if (actual_obs.x < player_x && actual_obs.x + actual_obs.width > player_x) {
-        if (actual_obs.y < player_y && actual_obs.y + actual_obs.height > player_y) {
-            action_obstacule[0] = 1;
-            action_obstacule[1] = 1;
         }
     }
-    return action_obstacule;
 }
 
+Objective create_action_obstacule(Rectangle actual_obs, float player_x, float player_y) {
+    Objective action_obstacule;
+    action_obstacule.x = player_x + (rand() % 100) - 50;
+    action_obstacule.y = player_y;
+    return action_obstacule;   
+}
+
+float* calculate_vel_x_and_vel_y_of_the_obstacule(Objective action_obstacule, float player_x, float player_y) {
+    float* vel_x_and_vel_y_of_the_obstacule = (float*) malloc(sizeof(float) * 2);
+    float vel_x_and_vel_y_of_the_obstacule_x = action_obstacule.x - player_x;
+    float vel_x_and_vel_y_of_the_obstacule_y = action_obstacule.y - player_y;
+    vel_x_and_vel_y_of_the_obstacule[0] = vel_x_and_vel_y_of_the_obstacule_x;
+    vel_x_and_vel_y_of_the_obstacule[1] = vel_x_and_vel_y_of_the_obstacule_y;
+    return vel_x_and_vel_y_of_the_obstacule;
+}
+
+Obstacule reset_position_of_the_obstacule(Obstacule obstacules) {  
+    obstacules.rect.x = 900;
+    obstacules.rect.y = 300;
+    obstacules.objective.x = -1;
+    obstacules.objective.y = -1;
+    obstacules.is_active = 1;
+    return obstacules;
+}
 
 void test_obstacules() {
     Rectangle* obstacules_obstacules = obstacules_init(5);
     int obstacules_number_of_obstacules = 5;
     render_obstacules(obstacules_obstacules, obstacules_number_of_obstacules);
 }
+Rectangle* return_all_rectangles_of_obstacules(Obstacule* obstacules_obstacules, int obstacules_number_of_obstacules) {
+    Rectangle* all_rectangles_of_obstacules = NULL;
+    all_rectangles_of_obstacules = (Rectangle*) malloc(sizeof(Rectangle) * obstacules_number_of_obstacules);
+    
+    for (int i = 0; i < obstacules_number_of_obstacules; i++) {
+        all_rectangles_of_obstacules[i] = obstacules_obstacules[i].rect;
+    }
+    return all_rectangles_of_obstacules;
+}
 
-void update_obstacules(Rectangle* obstacules_obstacules, int obstacules_number_of_obstacules) {
+void update_obstacules(Obstacule* obstacules_obstacules, int obstacules_number_of_obstacules) {
     int r = 0;
     for (int i = 0; i < obstacules_number_of_obstacules; i++) {
-        r = rand() % 4;
-        // if(r == 0) {
-            obstacules_obstacules[i].x -= r;
-            obstacules_obstacules[i].y += r;
 
-        // }
-    
+        if(obstacules_obstacules[i].rect.y > 700) { 
+            obstacules_obstacules[i] = reset_position_of_the_obstacule(obstacules_obstacules[i]);
+        }
+        else {
+            // if(i == 1) {
+                // printf("pos y: %f, pox x: %f,is_ativate: %d\n", obstacules_obstacules[i].rect.y, obstacules_obstacules[i].rect.x ,obstacules_obstacules[i].is_active);
+            // }
+            obstacules_obstacules[i].is_active = 1;
+            
+            if(obstacules_obstacules[i].is_active == 1) {
+                obstacules_obstacules[i].rect.y += rand() % 2;
+            }
+        }
+        // obstacules_obstacules[i].x -= r;
+        // obstacules_obstacules[i].y += r;    
     }
     render_obstacules(obstacules_obstacules, obstacules_number_of_obstacules);
 }
