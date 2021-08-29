1a02d381ebd6db4f34431881b4023d8ebe1cccba
Breno Cavalcanti <breno.uc.cavalcanti@gmail.com>
2021-08-29 15:20:44 -0300

    unifiation: resolvido problemas e implementação de items

--- src/modules/items/items.c
+++ src/modules/items/items.c
@@ -6,18 +6,18 @@
 typedef struct {
     float x;
     float y;
-} Objective;
+} ObjectiveItems;
 
 typedef struct {
     Rectangle rect;
-    Objective objective;
+    ObjectiveItems objective;
     int is_active;
 } Items;
 
 Items* itemsInit(int number_of_max_items) {
     Items* items = malloc(sizeof(Items) * number_of_max_items);
     for (int i = 0; i < number_of_max_items; i++) {
-        items[i].rect.x = 900;
+        items[i].rect.x = 500;
         items[i].rect.y = 10 *(i + 1);
         items[i].rect.width = 40;
         items[i].rect.height = 40;
@@ -28,86 +28,86 @@ Items* itemsInit(int number_of_max_items) {
     return items;    
 }
 
-void render_items(Items* obstacules_obstacules, int obstacules_number_of_obstacules,  Texture2D* obstacules_texture_2d) {
-    for (int i = 0; i < obstacules_number_of_obstacules; i++) {
-        DrawTexture(obstacules_texture_2d[i], obstacules_obstacules[i].rect.x, obstacules_obstacules[i].rect.y, WHITE);
+void render_items(Items* items_items, int items_number_of_items,  Texture2D* items_texture_2d) {
+    for (int i = 0; i < items_number_of_items; i++) {
+        DrawTexture(items_texture_2d[i], items_items[i].rect.x, items_items[i].rect.y, WHITE);
     }
 }
 
-Objective create_action_obstacule(Rectangle actual_obs, float player_x, float player_y) {
-    Objective action_obstacule;
-    action_obstacule.x = player_x + (rand() % 100) - 50;
-    action_obstacule.y = player_y;
-    return action_obstacule;   
+ObjectiveItems create_action_items(Rectangle actual_obs, float player_x, float player_y) {
+    ObjectiveItems action_items;
+    action_items.x = player_x + (rand() % 100) - 50;
+    action_items.y = player_y;
+    return action_items;   
 }
 
-float* calculate_vel_x_and_vel_y_of_the_obstacule(Objective action_obstacule, float player_x, float player_y) {
-    float* vel_x_and_vel_y_of_the_obstacule = (float*) malloc(sizeof(float) * 2);
-    float vel_x_and_vel_y_of_the_obstacule_x = action_obstacule.x - player_x;
-    float vel_x_and_vel_y_of_the_obstacule_y = action_obstacule.y - player_y;
-    vel_x_and_vel_y_of_the_obstacule[0] = vel_x_and_vel_y_of_the_obstacule_x;
-    vel_x_and_vel_y_of_the_obstacule[1] = vel_x_and_vel_y_of_the_obstacule_y;
-    return vel_x_and_vel_y_of_the_obstacule;
+float* calculate_vel_x_and_vel_y_of_the_items(ObjectiveItems action_items, float player_x, float player_y) {
+    float* vel_x_and_vel_y_of_the_items = (float*) malloc(sizeof(float) * 2);
+    float vel_x_and_vel_y_of_the_items_x = action_items.x - player_x;
+    float vel_x_and_vel_y_of_the_items_y = action_items.y - player_y;
+    vel_x_and_vel_y_of_the_items[0] = vel_x_and_vel_y_of_the_items_x;
+    vel_x_and_vel_y_of_the_items[1] = vel_x_and_vel_y_of_the_items_y;
+    return vel_x_and_vel_y_of_the_items;
 }
 
-Items reset_position_of_the_obstacule(Items obstacules) {  
-    obstacules.rect.x = 900;
-    obstacules.rect.y = 20;
-    obstacules.objective.x = -1;
-    obstacules.objective.y = -1;
-    obstacules.is_active = 1;
-    return obstacules;
+Items reset_position_of_the_items(Items items) {  
+    items.rect.x = 900;
+    items.rect.y = 20;
+    items.objective.x = -1;
+    items.objective.y = -1;
+    items.is_active = 1;
+    return items;
 }
 
 
-Rectangle* return_all_rectangles_of_obstacules(Items* obstacules_obstacules, int obstacules_number_of_obstacules) {
-    Rectangle* all_rectangles_of_obstacules = NULL;
-    all_rectangles_of_obstacules = (Rectangle*) malloc(sizeof(Rectangle) * obstacules_number_of_obstacules);
+Rectangle* return_all_rectangles_of_items(Items* items_items, int items_number_of_items) {
+    Rectangle* all_rectangles_of_items = NULL;
+    all_rectangles_of_items = (Rectangle*) malloc(sizeof(Rectangle) * items_number_of_items);
     
-    for (int i = 0; i < obstacules_number_of_obstacules; i++) {
-        all_rectangles_of_obstacules[i] = obstacules_obstacules[i].rect;
+    for (int i = 0; i < items_number_of_items; i++) {
+        all_rectangles_of_items[i] = items_items[i].rect;
     }
-    return all_rectangles_of_obstacules;
+    return all_rectangles_of_items;
 }
 
-Image* obstacules_image(int obstacules_number_of_obstacules) {
-    Image* obstacules_image = (Image*) malloc(sizeof(Image) * obstacules_number_of_obstacules);
-    obstacules_image[0] = LoadImage("./src/asserts/items/.png");
-    obstacules_image[1] = LoadImage("src/asserts/items/.png");
-    obstacules_image[2] = LoadImage("src/asserts/items/.png");
+Image* items_image(int items_number_of_items) {
+    Image* items_image = (Image*) malloc(sizeof(Image) * items_number_of_items);
+    items_image[0] = LoadImage("./src/asserts/items/item_vesiculo.png");
+    // items_image[1] = LoadImage("src/asserts/items/.png");
+    // items_image[2] = LoadImage("src/asserts/items/.png");
 
-    return obstacules_image;
+    return items_image;
 };
 
-Texture2D* obstacules_texture_2d(int obstacules_number_of_obstacules, Image* obstacules_texture) {
-    Texture2D* obstacules_texture_2d = (Texture2D*) malloc(sizeof(Texture2D) * obstacules_number_of_obstacules);
-    for (int i = 0; i < obstacules_number_of_obstacules; i++) {
-        obstacules_texture_2d[i] = LoadTextureFromImage(obstacules_texture[i]);
+Texture2D* items_texture_2d(int items_number_of_items, Image* items_texture) {
+    Texture2D* items_texture_2d = (Texture2D*) malloc(sizeof(Texture2D) * items_number_of_items);
+    for (int i = 0; i < items_number_of_items; i++) {
+        items_texture_2d[i] = LoadTextureFromImage(items_texture[i]);
     }
-    return obstacules_texture_2d;
+    return items_texture_2d;
 }
 
-void update_items(Items* obstacules_obstacules, int obstacules_number_of_obstacules, Texture2D* obstacules_texture_2d) {
+void update_items(Items* items_items, int items_number_of_items, Texture2D* items_texture_2d) {
     int r = 0;
-    for (int i = 0; i < obstacules_number_of_obstacules; i++) {
+    for (int i = 0; i < items_number_of_items; i++) {
 
-        if(obstacules_obstacules[i].rect.y > 700) { 
-            obstacules_obstacules[i] = reset_position_of_the_obstacule(obstacules_obstacules[i]);
+        if(items_items[i].rect.y > 700) { 
+            items_items[i] = reset_position_of_the_items(items_items[i]);
         }
         else {
-            obstacules_obstacules[i].is_active = 1;
+            items_items[i].is_active = 1;
             
-            if(obstacules_obstacules[i].is_active == 1) {
-                obstacules_obstacules[i].rect.y += rand() % 5;
-                // obstacules_obstacules[i].rect.x -= rand() % 5;
+            if(items_items[i].is_active == 1) {
+                items_items[i].rect.y += rand() % 5;
+                // items_items[i].rect.x -= rand() % 5;
             }
         }
 
     }
-    render_obstacules(obstacules_obstacules, obstacules_number_of_obstacules, obstacules_texture_2d);
+    render_items(items_items, items_number_of_items, items_texture_2d);
 }
 
-void init_module_obstacules() {
-    Rectangle* obstacules_obstacules = obstacules_init(5);
-    int obstacules_number_of_obstacules = 5;
+void init_module_items() {
+    Rectangle* items_items = itemsInit(5);
+    int items_number_of_items = 5;
 }
---
+++ src/modules/items/items.h
@@ -0,0 +1,16 @@
+#ifndef ITEMS_H
+#define ITEMS_H
+#include "items.c"
+
+Items* itemsInit(int number_of_max_items);
+void render_items(Items* items_items, int items_number_of_items,  Texture2D* items_texture_2d);
+ObjectiveItems create_action_items(Rectangle actual_obs, float player_x, float player_y);
+float* calculate_vel_x_and_vel_y_of_the_items(ObjectiveItems action_items, float player_x, float player_y);
+Items reset_position_of_the_items(Items items);
+Rectangle* return_all_rectangles_of_items(Items* items_items, int items_number_of_items);
+Image* items_image(int items_number_of_items);
+Texture2D* items_texture_2d(int items_number_of_items, Image* items_texture);
+void update_items(Items* items_items, int items_number_of_items, Texture2D* items_texture_2d);
+void init_module_items();
+
+#endif
